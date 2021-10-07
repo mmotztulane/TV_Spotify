7b37a4dcd0cfd565fae116840cd00a58ea4bb246
dudey dude <64184823+mmotztulane@users.noreply.github.com>
2021-10-07 12:43:15 +0000

    Added Table Mockups

--- tables.sql
+++ tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE users(username TEXT, password TEXT, name TEXT, birthday DATE);
 
 CREATE TABLE user(follower INTEGER, followerName TEXT, following INTEGER, followingName TEXT);
 
-CREATE TABLE userMovies(movieName TEXT, rating TEXT);
+CREATE TABLE userMovies(movieName TEXT, rating TEXT, comment TEXT);
 
 CREATE TABLE playlists(movie object);
 
