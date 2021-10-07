0b15cfa3f9aa628b3217116115bf350a5ce11bd5
dudey dude <64184823+mmotztulane@users.noreply.github.com>
2021-10-07 13:06:26 +0000

    Fleshed out SQL - Max

--- tables.sql
+++ tables.sql
@@ -1,9 +1,29 @@
-CREATE TABLE users(username TEXT, password TEXT, name TEXT, birthday DATE);
+CREATE TABLE users(ID INTEGER PRIMARY KEY, username TEXT, password TEXT, birthday DATE);
 
-CREATE TABLE user(follower INTEGER, followerName TEXT, following INTEGER, followingName TEXT);
+INSERT INTO users VALUES (1, "Max25","redacted", 2001-04-08);
+
+SELECT * FROM users
+
+CREATE TABLE friends(Follower ID # INTEGER PRIMARY KEY, followerName TEXT);
+
+INSERT INTO friends VALUES (2, "SanjoBanjo");
+
+SELECT * FROM friends
 
 CREATE TABLE userMovies(movieName TEXT, rating TEXT, comment TEXT);
 
-CREATE TABLE playlists(movie object);
+INSERT INTO userMovies VALUES ("Shrek", "like", "Entertaining throughout");
 
-CREATE TABLE movie(movieName TEXT, length INTEGER, genre TEXT, rating TEXT);
+SELECT * FROM userMovies
+
+CREATE TABLE playlists(movieName TEXT);
+
+INSERT INTO playlists VALUES ("Shrek");
+
+SELECT * FROM playlists
+
+CREATE TABLE movies(movieName TEXT, length_in_minutes INTEGER, genre TEXT, rating TEXT);
+
+INSERT INTO movies VALUES ("Shrek", 90, "Fantasy, Adventure, Comedy, Animated", "Like");
+
+SELECT * FROM movies
