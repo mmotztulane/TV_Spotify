e55c4b05b5fab841af363bc82a348f19b6434d2f
ozbutler12 <77716198+ozbutler12@users.noreply.github.com>
2021-10-07 03:31:40 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,89 @@
+# Milestone 0
+# group member: Owen Butler, Max Motz, Yuxuan Zhang
+# Project: TV Spotify
+
+
+Class Account
+
+  def initialize(user_name, password, birthdate)
+    @name = user_name
+    @password = password
+    @birthdate = birthdate
+    @followers = []         #list of users' followers
+    @following = []         #list of users' following
+  end
+
+  def setName(name)
+    @name = name
+  end
+
+  def setPassword(password)
+    @password = password
+  end
+
+  def setBirthDate(birthdate)
+    @birthday = birthDate
+  end
+
+  def follow(following_name)
+    @following << following_name
+  end
+
+  def getNumOfFollowing()
+    return @following.length
+  end
+
+  def followBy(name)
+    @followers << name
+  end
+
+  def getNumOfFollowers()
+    return @followers.length
+  end
+
+  def like(movie)
+    movie.likeBy(@name)
+  end
+
+  def dislike(movie)
+    movie.dislikeBy(@name)
+  end
+
+  def comment(movie,text)
+    movie.commentBy(text)
+  end
+
+end
+
+Class Movies
+
+  def initialize()
+    @like_list = []     #list of users who like it
+    @dislike_list = []  #list of users who dislike it 
+    @comments = []      #list comment written by user
+  end
+
+  def likeBy(name)      
+    @like_list << name
+  end
+
+  def dislikeBy(name)
+    @dislike_list << name
+  end
+
+  def commentBy(text)
+    @comments << text
+  end
+
+  def getNumOfLike()
+    return @like_list.length
+  end
+
+  def getNumOfDislike()
+    return @dislike_list.length
+  end
+
+end
+
+list_of_user = []
+list_of_movie = []
---
+++ tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users(username TEXT, password TEXT, name TEXT, birthday DATE);
+
+CREATE TABLE user(follower INTEGER, followerName TEXT, following INTEGER, followingName TEXT);
+
+CREATE TABLE userMovies(movieName TEXT, rating TEXT);
+
+CREATE TABLE playlists(movie object);
+
+CREATE TABLE movie(movieName TEXT, length INTEGER, genre TEXT, rating TEXT);
