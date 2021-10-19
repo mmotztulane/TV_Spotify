03e3a9421828252c41cafe63e36bfb68c94d2204
mmotztulane <mmotz@tulane.edu>
2021-10-19 04:31:25 +0000

    Bootstrap Forms part 1

--- spotifytv/db/migrate/20211007151015_create_users.rb
+++ spotifytv/db/migrate/20211007151015_create_users.rb
@@ -3,8 +3,13 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table 'users' do |t|
       t.string 'username'
       t.string 'reaction'
-      t.text 'comment'
+      t.text 'comment', :null => false, :default => 'No Comment'
       t.timestamps
     end
   end
+    
+    before_save :default_values
+    def default_values
+      self.comments= 'No Comment' if self.status.nil
+    end
 end
--- spotifytv/db/migrate/20211018074442_create_movies.rb
+++ spotifytv/db/migrate/20211018074442_create_movies.rb
@@ -2,13 +2,20 @@ class CreateMovies < ActiveRecord::Migration[6.0]
   def change
     create_table :movies do |t|
       t.string 'movie'
-      t.integer 'likes'
-      t.integer 'dislikes'
-      t.integer 'comments'
+      t.integer 'likes', :null => false, :default => '0'
+      t.integer 'dislikes', :null => false, :default => '0'
+      t.integer 'comments', :null => false, :default => '0'
       t.string 'genre'
       t.string 'age_rating'
       t.date 'release_date'
       t.timestamps
     end
   end
+    
+    before_save :default_values
+    def default_values
+      self.likes = '0' if self.status.nil
+      self.dislikes = '0' if self.status.nil
+      self.comments = '0' if self.status.nil
+    end
 end
