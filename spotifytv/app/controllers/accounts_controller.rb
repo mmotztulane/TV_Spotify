a05f9a768c4a9bbd76a4ea8433f9805c6d42d56c
Max Motz <mmotz@tulane.edu>
2021-11-04 21:04:09 +0000

    bug fixes

--- spotifytv/app/controllers/accounts_controller.rb
+++ spotifytv/app/controllers/accounts_controller.rb
@@ -44,7 +44,7 @@ class AccountsController < ApplicationController
     def destroy
         @account.destroy
         respond_to do |format|
-          format.html { redirect_to account_url, notice: "Account was successfully destroyed." }
+          format.html { redirect_to accounts_url, notice: "Account was successfully destroyed." }
           format.json { head :no_content }
         end
   end
--- spotifytv/db/seeds.rb
+++ spotifytv/db/seeds.rb
@@ -8,32 +8,46 @@
 more_users = [
   {:username => 'Max25', :reaction => 'Like',
     :comment => 'Entertaining throughout!'},
+    
   {:username => 'SanjoBanjo', :reaction => 'Like',
     :comment => 'Very Funny'},
+    
   {:username => 'Randy', :reaction => 'Dislike',
-    :comment => 'No comment'}
+    :comment => 'No comment'},
+    
     {:username => 'Sashon', :reaction => 'Like',
     :comment => 'I had a good time'},
+    
   {:username => 'Bashon', :reaction => 'Like',
     :comment => 'Dope!'},
+    
   {:username => 'lack', :reaction => 'Dislike',
-    :comment => 'No.'}
+    :comment => 'No.'},
+    
     {:username => 'lack', :reaction => 'Like',
     :comment => 'Good movie for Kids!'},
+    
   {:username => 'tack', :reaction => 'Like',
     :comment => 'Cool!'},
+    
   {:username => 'pack', :reaction => 'Dislike',
-    :comment => 'No comment'}
+    :comment => 'No comment'},
+    
   {:username => 'pack2', :reaction => 'Dislike',
-    :comment => 'Nada'}
+    :comment => 'Nada'},
+    
   {:username => 'knack', :reaction => 'Dislike',
-    :comment => 'Meh...'}
+    :comment => 'Meh...'},
+    
   {:username => 'jack', :reaction => 'Like',
-    :comment => 'Alright!'}
+    :comment => 'Alright!'},
+    
   {:username => 'hack', :reaction => 'Dislike',
-    :comment => 'Boo'}
+    :comment => 'Boo'},
+    
   {:username => 'sauron', :reaction => 'Like',
-    :comment => 'Fun!'}
+    :comment => 'Fun!'},
+    
   {:username => 'morepeople', :reaction => 'Like',
     :comment => 'Great movie!'}
 ]
@@ -50,10 +64,60 @@ more_movies = [
     :release_date => '11/02/2007'},
     
   {:movie => 'Joker', :likes => "5", :dislikes => "3", :comments => "16", :genre => "Thriller, Crime Film, Drama", :age_rating => "R", 
-    :release_date => '10/04/2019'},
+    :release_date => '10/04/2019'}
+ ]
+    
+  <<-DOC
+    
+  {:movie => 'Dune', :likes => "0", :dislikes => "0", :comments => "0", :genre => "Sci-Fi", :age_rating => "PG-13", 
+    :release_date => '10/22/2021'},
+    
+    {:movie => 'Inception', :likes => "100", :dislikes => "1", :comments => "3", :genre => "Drama, Heist", :age_rating => "PG-13", 
+    :release_date => '06/13/2010'},
+    
+  {:movie => 'Dodgeball', :likes => "7", :dislikes => "10", :comments => "10", :genre => "Comedy", :age_rating => "PG-13", 
+    :release_date => '06/18/2004'},
+    
+  {:movie => 'Shrek 2', :likes => "99999", :dislikes => "0", :comments => "21", :genre => "Comedy, Fantasy, Animated", :age_rating => "G", 
+    :release_date => '11/04/2021'},
+    
   {:movie => 'Shrek the Third', :likes => "0", :dislikes => "96", :comments => "0", :genre => "Comedy, Fantasy, Animated", :age_rating => "G", 
+    :release_date => '5/18/2007'},
+    
+    {:movie => 'Happy Feet 2', :likes => "0", :dislikes => "1000000", :comments => "999", :genre => "Animated", :age_rating => "G", 
+    :release_date => '11/18/2011'},
+    
+  {:movie => 'Happy Feet', :likes => "1", :dislikes => "500", :comments => "100", :genre => "Animated", :age_rating => "G", 
+    :release_date => '11/17/2006'},
+    
+  {:movie => 'Armagedon', :likes => "5", :dislikes => "3", :comments => "16", :genre => "Thriller, Drama", :age_rating => "R", 
+    :release_date => '10/04/2019'},
+    
+  {:movie => 'Bolt', :likes => "0", :dislikes => "96", :comments => "0", :genre => "Animated", :age_rating => "G", 
+    :release_date => '5/18/2007'},
+    {:movie => 'The Nightmare Before Christmas', :likes => "2", :dislikes => "1", :comments => "3", :genre => "Festive, Drama", :age_rating => "G", 
+    :release_date => '04/22/2001'},
+    
+  {:movie => 'Shark Tale', :likes => "1", :dislikes => "1", :comments => "10", :genre => "Animated", :age_rating => "G", 
+    :release_date => '11/02/2007'},
+    
+  {:movie => 'Jaws', :likes => "5", :dislikes => "3", :comments => "16", :genre => "Thriller", :age_rating => "R", 
+    :release_date => '10/04/2019'},
+  {:movie => 'Billy Madison', :likes => "0", :dislikes => "96", :comments => "0", :genre => "Comedy", :age_rating => "PG-13", 
+    :release_date => '5/18/2007'},
+    {:movie => 'Happy Gilmore', :likes => "2", :dislikes => "1", :comments => "3", :genre => "Comedy", :age_rating => "PG-13", 
+    :release_date => '04/22/2001'},
+    
+  {:movie => 'Madagascar', :likes => "1", :dislikes => "1", :comments => "10", :genre => "Animated", :age_rating => "G", 
+    :release_date => '11/02/2007'},
+    
+  {:movie => 'Madagascar 2', :likes => "5", :dislikes => "3", :comments => "16", :genre => "Animated", :age_rating => "G", 
+    :release_date => '10/04/2019'},
+    
+  {:movie => 'Madagascar 3', :likes => "0", :dislikes => "96", :comments => "0", :genre => "Animated", :age_rating => "G", 
     :release_date => '5/18/2007'}
-]
+    DOC
+
 
 
 
