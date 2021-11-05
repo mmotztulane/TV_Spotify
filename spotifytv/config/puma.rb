9af13f7a3599f1875467a9334928c9f83e3d6222
Max Motz <mmotz@tulane.edu>
2021-11-05 00:03:15 +0000

    adjustments to puma

--- spotifytv/config/puma.rb
+++ spotifytv/config/puma.rb
@@ -34,5 +34,11 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 #
 # preload_app!
 
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
+
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
