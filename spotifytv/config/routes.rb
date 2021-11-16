fcfbbed99f4473ce26b5bd2ae7a5d36e2055d17d
mmotztulane <mmotz@tulane.edu>
2021-11-16 05:02:22 +0000

    Working Authentication through devise

--- spotifytv/config/routes.rb
+++ spotifytv/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :logins
   get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
--- spotifytv/db/schema.rb
+++ spotifytv/db/schema.rb
@@ -10,13 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_223036) do
+ActiveRecord::Schema.define(version: 2021_11_16_041825) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "logins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_logins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_logins_on_reset_password_token", unique: true
   end
 
   create_table "movies", force: :cascade do |t|
