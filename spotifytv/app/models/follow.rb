b1fb2f715f648f2253fdb6fcdc22416939f6ba66
Max Motz <mmotz@tulane.edu>
2021-12-14 01:47:05 +0000

    I guess both my teammates broke it, damn

--- spotifytv/app/models/follow.rb
+++ spotifytv/app/models/follow.rb
@@ -1,7 +1,9 @@
 class Follow < ApplicationRecord
     #user giving the follow
-    belongs_to:follower, foreign_key: :follower_id, class_name:"User"
+    #belongs_to:follower, foreign_key: :follower_id, class_name:"User"
     
     #The user being followed
-    belongs_to:followed_user, foreign_key::followed_user_id, class_name: "User"
+    #belongs_to:followed_user, foreign_key::followed_user_id, class_name: "User"
+    
+    #Currently not allowing me to push to heroku, so commented out
 end
