7143ee45e5461ba287506d1a6387dad7773b8a7b
Max Motz <mmotz@tulane.edu>
2021-12-14 18:08:06 +0000

    debugging heroku

--- spotifytv/app/javascript/packs/application.js
+++ spotifytv/app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('image-enlarge')
 
 document.on('turbolinks:load', alert("Website has sucessfully loaded!"));
 
